5a8cd8873e3409f4a214ce8c586c9da9e6ba8a74
nurnobi <shuvo517942@gmail.com>
2021-03-28 12:50:29 +0600

    AdminLte-3 adding and override collectstatic v1

--- Washa/Washa/settings.py
+++ Washa/Washa/settings.py
@@ -35,6 +35,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'adminlte3',
+    'adminlte3_theme',
+    #'django_admin_bootstrapped',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
